a8e14f91de2fe77c9183b47f0ca8aaa74fd9230f
Az <arbusinos@up.edu.ph>
2024-04-28 00:20:59 +0800

    changed user models. not final. separated abugadoacc and abugado models.

--- abugado/models.py
+++ abugado/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class AbugadoAcc(models.Model):
+    account_created = models.DateField(auto_now_add=True)
+    email = models.EmailField(primary_key=True)
+    password = models.CharField(max_length=200)
+    # optional! needs new library (Pillow) profile_pic = models.ImageField()
+
 class Abugado(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     license_number = models.CharField(max_length=200)
     sex = models.CharField(max_length=2)
     contact_number = models.CharField(max_length=12)
-    email_add = models.CharField(max_length=200)
+    abugado_acc = models.OneToOneField(to=AbugadoAcc, on_delete=models.CASCADE, primary_key=True)
     
 class Cases(models.Model):
     abugado = models.ForeignKey(to=Abugado, on_delete=models.CASCADE)
