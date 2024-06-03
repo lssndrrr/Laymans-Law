0b980ada33b21e5fe77eb591f1423c630acc17a0
Az <arbusinos@up.edu.ph>
2024-06-03 17:42:09 +0800

    changes

---
+++ manggagawa/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+from datetime import date
+
+
+## VALIDATORS
+
+def validate_age(bdate):
+    today = date.today()
+    if today.year - bdate.year - ((today.month, today.day) < (bdate.month, bdate.day)) >= 15:
+        return bdate
+    else:
+        raise ValidationError("You have to be 15 to sign up.")
