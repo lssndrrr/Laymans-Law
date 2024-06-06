b70a7cb02473729edf1819c7d26acfc386bb2b64
Az <arbusinos@up.edu.ph>
2024-06-06 10:47:32 +0800

    [az] refactor: admindatabase

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,16 +3,9 @@ from .models import Law, Translations, Summarizations
 
 # Register your models here.
 
-@admin.register(Law)
+@admin.register(Law, Translations, Summarizations)
 class LawAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Summarizations)
-class SummarizationsAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(Translations)
-class TranslationsAdmin(admin.ModelAdmin):
-    pass
 
 # SUGGESTIONS, SMTH LIKE TIKTOK. PREDICTIVE ON WHAT CASES TO TAKE
